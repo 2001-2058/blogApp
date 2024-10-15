77263f056ba9b98984a7e9272f5b959eab0069d0
Utsav Shakya <149353437+2001-2058@users.noreply.github.com>
2024-10-15 13:01:34 +0545

    Add files via upload

--- postgres/postgres.js
+++ postgres/postgres.js
@@ -1,19 +1,25 @@
 import { Sequelize } from "sequelize";
+import { createUserModel } from "../model/userSchema.js";
 
-const sequelize = new Sequelize('postgres', 'root', 'root', {
+const sequelize = new Sequelize('postgres', 'postgres', '12345', {
     host: 'localhost',
-    dialect:'postgres'
+    dialect: 'postgres'
   });
 
+  let UserModel=null;
   const connection=async()=>{
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+        UserModel= await createUserModel(sequelize)
+        await sequelize.sync();
+        console.log("Database Synced");
       } catch (error) {
         console.error('Unable to connect to the database:', error);
       }
   }
 
-  export {
-    connection
+  export{
+    connection,
+    UserModel
   }
