37a990f5c218995c28318e1ddd3835d29226adae
Utsav Shakya <149353437+2001-2058@users.noreply.github.com>
2024-10-24 13:30:54 +0545

    Add files via upload

--- view/routes.js
+++ view/routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllEmp, addEmp, updateEmp, deleteEmp } from "../controller/userController.js";
+import { getAllEmp, addEmp, updateEmp, deleteEmp, uploadProfilePic, deleteProfilePic } from "../controller/userController.js";
 const router=express.Router();
 
 router.get("/getAll",getAllEmp);
@@ -7,5 +7,10 @@ router.post("/addEmp",addEmp);
 router.put("/emp/:empId",updateEmp);
 router.delete("/emp/:empId",deleteEmp);
 
+// Route to upload a profile picture
+router.post('/emp/uploadProfilePic/:empId', uploadProfilePic);
+
+// Route to delete the profile picture
+router.delete('/emp/deleteProfilePic/:empId', deleteProfilePic);
 
 export default router;
