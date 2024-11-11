c5c7f40d3a8841177c378ec1ac587bb521fe1c6d
Utsav Shakya <149353437+2001-2058@users.noreply.github.com>
2024-11-11 20:48:30 +0545

    Add files via upload

--- view/routes.js
+++ view/routes.js
@@ -1,14 +1,29 @@
-import express from "express";
+import express from 'express';
 import { getAllEmp, addEmp, updateEmp, deleteEmp, uploadProfilePic, deleteProfilePic } from "../controller/userController.js";
-const router=express.Router();
+import { upload } from "../middleware/fileUpload.js";
 
-router.get("/getAll",getAllEmp);
-router.post("/addEmp",addEmp);
-router.put("/emp/:empId",updateEmp);
-router.delete("/emp/:empId",deleteEmp);
+const router = express.Router();
+
+router.get("/getAll", getAllEmp);
+router.post("/addEmp", addEmp);
+router.put("/emp/:empId", updateEmp);
+router.delete("/emp/:empId", deleteEmp);
 
 // Route to upload a profile picture
-router.post('/emp/uploadProfilePic/:empId', uploadProfilePic);
+router.post('/emp/uploadProfilePic/:empId', upload.single('profilePic'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ message: 'No file uploaded.' });
+    }
+  
+    const empId = req.params.empId;
+    const filePath = req.file.path; // Path to the uploaded file
+  
+    res.status(200).json({
+      message: 'Profile picture uploaded successfully',
+      empId: empId,
+      filePath: filePath,
+    });
+  });
 
 // Route to delete the profile picture
 router.delete('/emp/deleteProfilePic/:empId', deleteProfilePic);
